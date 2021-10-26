119cf3525003611bc019d0a080ba04ec6530cf77
Anton Liubimov <ant.liubimov@gmail.com>
2021-10-26 16:29:02 +0500

    вынес директорию в отдельный компонент

---
+++ src/components/TreeDirectory/TreeDirectory.vue
@@ -0,0 +1,33 @@
+<template>
+  <div  @click="toggleDirectory" :class="['tree-item',{'bold': isOpen}]">
+    <img v-if="isOpen" src="../../assets/folder-open.svg" width="16" height="16" alt="directory">
+    <img v-else src="../../assets/folder-close.svg" width="16" height="16" alt="directory">
+    {{ TreeItemData.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TreeDirectory",
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+  props: {
+    TreeItemData: Object,
+  },
+  methods: {
+    toggleDirectory() {
+      this.isOpen = !this.isOpen;
+      this.$emit('toggle', this.isOpen);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .bold {
+    font-weight: bold;
+  }
+</style>
